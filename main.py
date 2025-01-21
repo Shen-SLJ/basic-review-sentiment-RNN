e8265695d670ecf14e004afb67c613e45e3c02a4
Shen-SLJ <shen.jng1@gmail.com>
2025-01-22 00:10:32 +1100

    Fix exploding loss value by replacing loss function

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ if __name__ == '__main__':
     vocab_size = 10000
     embedding_size = 128
     batch_size = 32
-    epochs = 25
+    epochs = 20
 
     (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=vocab_size)
     x_train = pad_sequences(sequences=x_train, maxlen=max_len)
     x_test = pad_sequences(sequences=x_test, maxlen=max_len)
 
     model = Sequential()
-    model.add(InputLayer(input_shape=(max_len,)))
+    model.add(InputLayer(shape=(max_len,)))
     model.add(Embedding(input_dim=vocab_size, output_dim=embedding_size))
-    model.add(SimpleRNN(units=embedding_size, activation='relu'))
+    model.add(SimpleRNN(units=embedding_size, activation='tanh'))
     model.add(Dense(units=1, activation='sigmoid'))
 
-    early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
 
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
